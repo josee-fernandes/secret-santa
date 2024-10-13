cf924bb8edc6b6303e5828d9282e3720abddbe3c
José Vitor <jose-vitor-7@hotmail.com>
2024-10-12 23:41:01 -0300

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { ThemeProvider } from '@/components/theme-provider'
+import { Toaster } from '@/components/ui/sonner'
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <Toaster
+        expand
+        richColors
+        closeButton
+        toastOptions={{
+          duration: 3000,
+          classNames: {
+            closeButton:
+              'right-0 left-[initial] translate-x-[35%] -translate-y-[35%]',
+          },
+        }}
+      />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
+
+App.displayName = 'App'
+
+export default App
